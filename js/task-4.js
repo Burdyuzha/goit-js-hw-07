6bd8ce88f439e5b2fc9752c12fa534ce81740223
Gregory Burdyuzha <grisha.burdyuzha3@gmail.com>
2026-04-10 20:10:50 +0300

    bug fix

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,4 @@
 const form = document.querySelector(".login-form");
-const values = {};
 form.addEventListener("submit", event => {
   event.preventDefault();
   const email = event.currentTarget.elements.email.value.trim();
@@ -15,11 +14,8 @@ form.addEventListener("submit", event => {
   if (missingFields.length > 0) {
     alert(`All form fields must be filled in ${missingFields.join(", ")}`);
   } else {
-    values.email = email;
-  values.password = password;
-  for (let key in values) {
-    console.log(`${key}:${values[key]}`);
-  }
+    
+    console.log(`Email:${email}\n Password:${password}`);
   form.reset();
   }
   
